e3225fb9ce6a6a3a57d3d9240d958f070343b06f
Günther Noack <guenther@unix-ag.uni-kl.de>
2022-01-11 21:59:33 +0100

    Document

--- future/future.go
+++ future/future.go
@@ -3,6 +3,7 @@ package future
 
 type Fulfil[T any] func(T)
 
+// New returns a new Future[T] and a function that fulfils it.
 func New[T any]() (*Future[T], Fulfil[T]) {
 	f := Future[T]{
 		done: make(chan struct{}),
@@ -10,6 +11,8 @@ func New[T any]() (*Future[T], Fulfil[T]) {
 	return &f, f.fulfil
 }
 
+// Go runs the given function f in a background goroutine,
+// and immediately returns a future that will be fulfilled when f returns.
 func Go[T any](f func() T) *Future[T] {
 	fut, fulfil := New[T]()
 	go func() {
@@ -19,11 +22,14 @@ func Go[T any](f func() T) *Future[T] {
 	return fut
 }
 
+// Future represents a value of T that will become available at some
+// point in the future.
 type Future[T any] struct {
 	v T
 	done chan struct{}
 }
 
+// Value returns the future's value, waiting for fulfilment if needed.
 func (f*Future[T]) Value() T {
 	<-f.done
 	return f.v
